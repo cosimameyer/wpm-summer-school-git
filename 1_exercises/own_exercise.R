c00fbcbe43e00369c3ac6297fc0fde8de80ea438
qm2025-mahuette <marietta.sujin@gmail.com>
2026-07-20 15:18:37 +0200

    doc: more commitment exercise

--- 1_exercises/own_exercise.R
+++ 1_exercises/own_exercise.R
@@ -1,1 +1,8 @@
 # This is a test
+
+# copying from EXAMPLE.R to exercise commits and more
+clean_colnames <- function(df) {
+  names(df) <- tolower(names(df))              # HEADLINE -> headline
+  names(df) <- gsub(" ", "_", names(df))       # "birth year" -> "birth_year"
+  df                                           # return the changed data frame
+}
